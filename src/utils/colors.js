496061399c0f2e43b8d968d604fe5ac100bdbbbe
codywall <cody.wall@gmail.com>
2019-11-21 13:10:37 -0800

    Added grey colors

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,7 +1,9 @@
 const colors = {
-  primary: '#0471a6',
-  white: '#fdfdfd',
-  accent: '#f0c564'
+  primary: 'rgba(4, 113, 166, 1)',
+  white: 'rgba(253, 253, 253, 1)',
+  accent: 'rgba(240, 197, 100, 1)',
+  lightgrey: 'rgba(176, 181, 179, 1)',
+  darkgrey: 'rgba(57, 62, 65, 1)'
 };
 
 export default colors;
