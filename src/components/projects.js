c4091bde2207779f87e776a27952a8f1476a5750
codywall <cwall@csumb.edu>
2019-11-21 16:35:44 -0800

    Made project cards responsive

--- src/components/projects.js
+++ src/components/projects.js
@@ -29,15 +29,20 @@ const projects = [
 ];
 
 const ProjectsWrapper = styled('div')`
-  background-color: #e8e8e8;
+  background-color: ${colors.lightgrey};
   width: 100vw;
   height: auto;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ProjectCard = styled('div')`
-  margin: 40px 5vw;
+  margin: -10px 2.5vw 50px 2.5vw;
+  width: 90vw;
+  min-height: 300px;
   background-color: ${colors.white};
   padding: 3vw;
   border-radius: 5px;
@@ -51,19 +56,26 @@ const ProjectCard = styled('div')`
   box-shadow: 0 35px 50px -30px rgba(0, 0, 0, 0.2);
 
   @media ${device.tablet} {
-    max-width: 800px;
+    width: 80vw;
+  }
+  @media ${device.laptop} {
+    width: 39vw;
   }
 `;
 
 const ImgWrapper = styled('div')`
-  width: 85vw;
-  height: 45vw;
+  width: 100%;
+  min-height: 200px;
   align-self: center;
   overflow: hidden;
   height: 300px;
   border-radius: 5px;
   position: relative;
   margin-bottom: 3vw;
+
+  @media ${device.tablet} {
+    width: 100%;
+  }
 `;
 
 const ProjectImage = styled('img')`
@@ -150,6 +162,12 @@ const displayProjects = projects.map(project => (
   </ProjectCard>
 ));
 
-const Projects = () => <ProjectsWrapper>{displayProjects}</ProjectsWrapper>;
+const Projects = () => (
+  <ProjectsWrapper>
+    {' '}
+    <h2 style={{ flex: '0 0 100%', padding: '2vw 10vw', fontSize: '2em' }}>Projects</h2>
+    {displayProjects}
+  </ProjectsWrapper>
+);
 
 export default Projects;
--- src/components/tools.js
+++ src/components/tools.js
@@ -13,9 +13,6 @@ const ToolsWrapper = styled('div')`
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-
-  @media ${device.tablet} {
-  }
 `;
 
 const ToolCard = styled('div')`
@@ -38,24 +35,6 @@ const ToolCard = styled('div')`
   }
 `;
 
-/* <div id="tools__wrapper">
-            <div id="tools__dev" class="tools__card">
-                <h4 class="card__header">Frontend Development</h4>
-                <p class="card__description">Using modern technologies to build fast, functional websites.
-                </p>
-                <div class="card__tech--wrapper">
-                    <p class="card__caption">Responsive design with JavaScript, HTML, CSS, SASS, Wordpress, and Git</p>
-                </div>
-            </div>
-            <div id="tools__design" class="tools__card">
-                <h4 class="card__header">User Interface Design</h4>
-                <p class="card__description">Solving design problems with the end user and usability in mind.</p>
-                <div class="card__tech--wrapper">
-                    <p class="card__caption">Wireframes, prototypes, logos, and motion graphics with the Adobe
-                        Suite.</p>
-                </div>
-            </div>
-        </div> */
 const Tools = () => (
   <ToolsWrapper>
     <ToolCard>
