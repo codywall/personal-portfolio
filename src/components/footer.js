70d8f78e3a8d0080a96f172ded9aa01d8bb9a047
codywall <cwall@csumb.edu>
2020-09-03 15:23:51 -0700

    Refactor footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,67 +6,44 @@ import colors from '../utils/colors';
 import device from '../utils/breakpoints';
 
 const FooterWrapper = styled('div')`
-  display: 'flex';
-  justify-content: 'space-between';
-  align-content: 'center';
-  position: 'fixed';
-  top: 0;
-  width: 100vw;
-  z-index: 499;
-  padding: 8px;
-  height: 68px;
-  transition: 0.3s;
-`;
-
-const FooterCard = styled('div')`
-  margin: -10px 2.5vw 50px 2.5vw;
-  width: 90vw;
-  min-height: 300px;
-  height: 100%;
   display: flex;
+  justify-content: center;
+  align-content: center;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  position: relative;
+  width: 100vw;
+  padding: 8px;
+  min-height: 68px;
+  text-align: center;
 
-  @media ${device.laptop} {
-    width: 800px;
+  h2 {
+    flex: 0 0 100%;
+    padding-top: 2rem;
+    font-size: 2rem;
+    color: white;
   }
 
-  @media ${device.mobile} {
-    min-height: 150px;
-  }
-`;
+  a {
+    text-decoration: underline;
+    padding-bottom: 2rem;
+    font-size: 1.3em;
+    font-weight: 600;
+    color: ${colors.accent};
 
-const ContactLink = styled('a')`
-  padding: 15px;
-  text-decoration: underline;
-  width: 70%;
-  font-size: 1.3em;
-  align-self: center;
-  color: ${colors.white};
-
-  @media ${device.mobile} {
-    width: 100%;
-    font-size: 1.1rem;
+    @media ${device.mobile} {
+      font-size: 1.1rem;
+    }
   }
 `;
 
 const Footer = () => (
-  <div>
-    <FooterWrapper>
-      <h2 style={{ flex: '0 0 100%', padding: '2vw 10vw', fontSize: '2em', color: 'white' }}>
-        Contact me
-      </h2>
-      <FooterCard>
-        <ContactLink href="mailto:cwall@csumb.edu">Email me at cody.wall@gmail.com</ContactLink>
-        <ContactLink href="https://github.com/codywall">Check out my GitHub</ContactLink>
-        <ContactLink href={withPrefix('/resume_codywall.pdf')} rel="noreferrer">
-          View my resume
-        </ContactLink>
-      </FooterCard>
-    </FooterWrapper>
-  </div>
+  <FooterWrapper>
+    <h2>Let&apos;s work together!</h2>
+    <a href="mailto:cwall@csumb.edu">cody.wall@gmail.com</a>
+    <a href="https://github.com/codywall">github.com/codywall</a>
+    <a href={withPrefix('/resume_codywall.pdf')} rel="noreferrer">
+      View my resume
+    </a>
+  </FooterWrapper>
 );
 
 export default Footer;
