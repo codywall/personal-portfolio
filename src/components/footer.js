4a8b67e1111dcf4be899a2994bbb946576e2068f
codywall <cody.wall@gmail.com>
2020-09-24 11:47:05 -0700

    Global styling changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import Line from './common/line';
 
@@ -7,16 +6,17 @@ const Copyright = styled('h6')`
   margin-top: 3rem;
   font-weight: 200;
   text-align: center;
-`
+`;
 
 const Footer = () => (
   <>
-    <Line/>
+    <Line />
     <h2>Get in touch with me</h2>
-    <p>
+    <p style={{ fontSize: '1.2rem' }}>
       If you would like to learn more about me, feel free to connect with me on
       <a href="https://linkedin.com/in/codycwall"> LinkedIn, </a>
-      and follow me on <a href="https://github.com/codywall">GitHub</a> to see what I&apos;m working on!
+      and follow me on <a href="https://github.com/codywall">GitHub</a> to see what I&apos;m working
+      on!
     </p>
     <Copyright>&#169;2020 Cody Wall</Copyright>
   </>
