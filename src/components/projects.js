32e2a485debb582b928528736f4db34e4594a689
codywall <cody.wall@gmail.com>
2020-09-23 21:12:35 -0700

    Refactor and simplify project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import Line from './common/line';
 import cowells from '../images/cowells-preview.png';
 import cody from '../images/cody-preview.png';
 import github from '../images/icons/github-logo.svg';
@@ -28,149 +29,50 @@ const projects = [
   }
 ];
 
-const ProjectsWrapper = styled('div')`
-  background-color: ${colors.lightgrey};
-  width: 100vw;
-  height: auto;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-`;
-
 const ProjectCard = styled('div')`
-  margin: -10px 2.5vw 50px 2.5vw;
-  width: 90vw;
-  min-height: 300px;
-  background-color: ${colors.white};
-  padding: 3vw;
   border-radius: 5px;
-  border: none;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  position: relative;
+  border: 1px solid rgba(0, 0, 0, 0.2);
   box-shadow: 0 35px 50px -30px rgba(0, 0, 0, 0.2);
+  margin: 2rem 0;
+  padding: 1rem;
 
-  @media ${device.tablet} {
-    width: 80vw;
-  }
-  @media ${device.laptop} {
-    width: 39vw;
-  }
-`;
+  .icons-wrapper {
+    display: flex;
+    justify-content: space-around;
+    fill: ${colors.primary};
+    width: 50%;
 
-const ImgWrapper = styled('div')`
-  width: 100%;
-  min-height: 200px;
-  align-self: center;
-  overflow: hidden;
-  height: 300px;
-  border-radius: 5px;
-  position: relative;
-  margin-bottom: 3vw;
-
-  @media ${device.tablet} {
-    width: 100%;
+    a img {
+      height: auto;
+      width: 50px;
+      margin-bottom: 0.5rem;
+    }
   }
 `;
-
-const ProjectImage = styled('img')`
-  position: absolute;
-  bottom: -60px;
-  height: 250%;
-  filter: brightness(55%);
-  width: auto;
-  object-fit: cover;
-`;
-
-const ProjectInfo = styled('div')`
-  z-index: 198;
-  display: flex;
-  flex-direction: column;
-  background-color: ${colors.primary};
-  box-shadow: 0 35px 50px -30px rgba(0, 0, 0, 0.2);
-  position: absolute;
-  top: 0;
-  padding: 10px;
-  width: 100%;
-  height: 24%;
-  color: ${colors.white};
-`;
-
-const ProjectHeadline = styled('h3')`
-  z-index: 199;
-  margin: 2px 5px 5px 5px;
-`;
-
-const ProjectType = styled('h5')`
-  z-index: 199;
-  margin: 0 5px 5px 5px;
-  font-size: 1rem;
-`;
-
-const IconWrapper = styled('div')`
-  display: flex;
-  fill: ${colors.primary};
-  justify-content: space-around;
-  width: 100%;
-
-  a img {
-    height: auto;
-    width: 50px;
-  }
-`;
-
-const Icon = styled('div')`
-  display: flex;
-  align-items: center;
-  top: -10px;
-  justify-content: center;
-  text-align: center;
-
-  img {
-    margin-bottom: 10px;
-  }
-`;
-
-const displayProjects = projects.map(project => (
-  <ProjectCard key={project.name}>
-    <ImgWrapper>
-      <a href={project.url}>
-        <ProjectInfo>
-          <ProjectHeadline>{project.name}</ProjectHeadline>
-          <ProjectType>{project.projectType}</ProjectType>
-        </ProjectInfo>
-        <ProjectImage src={`${project.imgUrl}`} alt={`${project.name}`} />
-      </a>
-    </ImgWrapper>
-    <p>{project.description}</p>
-    <IconWrapper>
-      <Icon>
-        <a href={project.githubUrl}>
-          <img src={github} alt="" />
-          <h5>Github</h5>
-        </a>
-      </Icon>
-      <Icon>
-        <a href={project.url}>
-          <img src={externalLink} alt="" />
-          <h5>Site Link</h5>
-        </a>
-      </Icon>
-    </IconWrapper>
-  </ProjectCard>
-));
 
 const Projects = () => (
-  <ProjectsWrapper>
-    {' '}
-    <h2 style={{ flex: '0 0 100%', padding: '2vw 10vw', fontSize: '2em' }}>Projects</h2>
-    {displayProjects}
-  </ProjectsWrapper>
+  <>
+    <Line />
+    <h2>Recent projects</h2>
+    {projects.map(project => (
+      <ProjectCard key={project.name}>
+        <a href={project.url}>
+          <h3>{project.name}</h3>
+        </a>
+        <p>{project.description}</p>
+        <div className="icons-wrapper">
+          <a href={project.githubUrl}>
+            <img src={github} alt="" />
+            <h5>Github</h5>
+          </a>
+          <a href={project.url}>
+            <img src={externalLink} alt="" />
+            <h5>Site Link</h5>
+          </a>
+        </div>
+      </ProjectCard>
+    ))}
+  </>
 );
 
 export default Projects;
