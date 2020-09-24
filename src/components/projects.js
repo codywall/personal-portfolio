4101691716009758b3ba5cdb9422bbeb777fad31
codywall <cody.wall@gmail.com>
2020-09-23 22:14:34 -0700

    Align link captions

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,11 +6,11 @@ import cody from '../images/cody-preview.png';
 import github from '../images/icons/github-logo.svg';
 import externalLink from '../images/icons/link.svg';
 import device from '../utils/breakpoints';
+import colors from '../utils/colors';
 
 const projects = [
   {
     name: "Cowell's Surf Shop",
-    projectType: 'Retail store website',
     description:
       'Currently building a website for a surf shop in Santa Cruz, CA. I am using React to pull surf report data from an API, and to pull content from a headless CMS.',
     url: 'https://cowells-prismic.netlify.com/',
@@ -18,8 +18,7 @@ const projects = [
     imgUrl: cowells
   },
   {
-    name: 'Cody Wall',
-    projectType: 'Personal portfolio',
+    name: 'Personal Portfolio',
     description:
       'Rebuilt my personal portfolio using React and Gatsby, in order to achieve faster loading times.',
     url: 'https://www.codywall.com',
@@ -30,8 +29,8 @@ const projects = [
 
 const ProjectCard = styled('div')`
   border-radius: 5px;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  box-shadow: 0 35px 50px -30px rgba(0, 0, 0, 0.2);
+  border: 1.5px solid ${colors.lightgrey};
+  box-shadow: 0 15px 20px -20px rgba(0, 0, 0, 0.2);
   margin: 2rem 0;
   padding: 1rem;
 
@@ -43,6 +42,12 @@ const ProjectCard = styled('div')`
 
     @media only screen and (${device.tablet}) {
       width: 50%;
+    }
+
+    .icon {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
 
     a img {
@@ -63,12 +68,16 @@ const Projects = () => (
         <p>{project.description}</p>
         <div className="icons-wrapper">
           <a href={project.githubUrl}>
-            <img src={github} alt="" />
-            <h5>Github</h5>
+            <div className="icon">
+              <img src={github} alt="" />
+              <h5>Source</h5>
+            </div>
           </a>
           <a href={project.url}>
-            <img src={externalLink} alt="" />
-            <h5>Site Link</h5>
+            <div className="icon">
+              <img src={externalLink} alt="" />
+              <h5>Site Link</h5>
+            </div>
           </a>
         </div>
       </ProjectCard>
