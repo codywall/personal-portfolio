cc0461e3fa4b2b0ec8860034db5daf60f846e096
codywall <cody.wall@gmail.com>
2020-09-23 21:54:04 -0700

    Add header to skills section

--- src/components/tools.js
+++ src/components/tools.js
@@ -4,13 +4,14 @@ import Line from './common/line';
 const Tools = () => (
   <>
     <Line />
-    <h3>Frontend Web Development</h3>
+    <h2>Skills</h2>
+    <h4>Frontend Web Development</h4>
     <p>
       I use modern technologies to build fast, functional websites. I have professional experience
       developing responsive websites and applications with JavaScript, React, HTML/CSS, GatsbyJS and
       Git.
     </p>
-    <h3>User Interface Design</h3>
+    <h4>User Interface Design</h4>
     <p>
       A strong background in UI/UX design guides my development process and allows me to view the
       applications I am creating from the perspective of the user.
