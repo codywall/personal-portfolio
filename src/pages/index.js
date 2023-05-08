da2426ba0e0befe5d28bb3d5224234da1877a686
codywall <cody.wall@gmail.com>
2023-05-08 10:43:05 -0700

    Abstract out json data from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,53 +9,14 @@ import {
   TextInput,
   Textarea,
   Button,
-  Anchor,
   Flex,
   Title
 } from '@mantine/core';
 
+import jobs from '../data/jobs.json';
+import featuredPosts from '../data/featured-posts.json';
+
 const IndexPage = () => {
-  // const { data } = props;
-  // const allProjects = data.allMarkdownRemark.edges;
-  const jobs = [
-    {
-      id: '1',
-      title: 'Database Administrator',
-      company: 'California State University, Monterey Bay',
-      dates: 'March 2023 - Present',
-      description:
-        "As a key member of the IT department, I play a critical role in managing and maintaining the university's Oracle databases and servers. I ensure data integrity, security, and optimal performance by implementing effective monitoring tools and applying best practices. My proactive approach to database management has helped the university improve efficiency and make data-driven decisions."
-    },
-    {
-      id: '2',
-      title: 'Senior Frontend Engineer',
-      company: 'Titan.io',
-      dates: 'February 2022 - March 2023',
-      description:
-        "At Titan.io, I've been instrumental in driving the company's success through multiple high-impact projects. I led the development of marketing pages for Lumerin Hashpower Marketplace's beta launch, resulting in an impressive 12,000+ signups within the first week. By revamping the Lumerin Wallet UI, I significantly enhanced the user experience. Additionally, I played a pivotal role in unifying the Titan and Lumerin brands, creating a stronger visual identity across platforms."
-    },
-    {
-      id: '3',
-      title: 'Software Developer',
-      company: 'California State University, Monterey Bay',
-      dates: 'October 2019 - February 2022',
-      description:
-        "During my tenure as a Software Developer, I made substantial contributions to the university's digital infrastructure. I implemented a user-friendly dashboard for potential students, leveraging Salesforce to streamline the application process and enhance applicant satisfaction. Collaborating with UX designers, I improved the campus website's functionality by adding features and integrating APIs and headless content management systems, enriching the overall user experience."
-    }
-  ];
-
-  const posts = [
-    {
-      title: 'Exploring the Future of Decentralization',
-      description:
-        'A deep dive into the potential impact of blockchain technology on various industries.'
-    },
-    {
-      title: 'Mastering Frontend Development',
-      description: 'Essential tips and tricks for creating stunning, user-friendly web interfaces.'
-    }
-  ];
-
   return (
     <Container size="sm" px="lg">
       <Container id="hero" py={50}>
@@ -116,6 +77,20 @@ const IndexPage = () => {
         </Flex>
       </Container>
 
+      <Container id="blog" pb={50}>
+        <Title order={2} align="center">
+          Blog
+        </Title>
+        <ul>
+          {featuredPosts.map(post => (
+            <li key={post.title}>
+              <Text weight={500}>{post.title}</Text>
+              <Text>{post.description}</Text>
+            </li>
+          ))}
+        </ul>
+      </Container>
+
       <Container pb={50}>
         <Title order={2} align="center">
           Work Experience
@@ -132,20 +107,6 @@ const IndexPage = () => {
             </Col>
           </Grid>
         ))}
-      </Container>
-
-      <Container id="blog" pb={50}>
-        <Title order={2} align="center">
-          Blog
-        </Title>
-        <ul>
-          {posts.map(post => (
-            <li key={post.title}>
-              <Text weight={500}>{post.title}</Text>
-              <Text>{post.description}</Text>
-            </li>
-          ))}
-        </ul>
       </Container>
 
       <Container id="contact">
